3ef9b6cf111e3056d04b2533fb1e1cb16073b8ad
Florian Muller-Fouarge <florian.mueller.fouarge@gmail.com>
2020-10-21 15:01:36 +0200

    update exceptions decorator

--- code_utils/decorators/exceptions.py
+++ code_utils/decorators/exceptions.py
@@ -1,15 +1,28 @@
-
+from contextlib import contextmanager
 
 __all__ = [
     'ignore_exception',
 ]
-def ignore_exception(function):
-    """
-    Decorator for supressing all exceptions
-    """
-    def wrapper(*func_args, **func_kwargs):
-        try:
-            return function(*func_args, **func_kwargs)
-        except:
-            pass
-    return wrapper
+
+# def ignore_exceptions(function):
+#     """
+#     Decorator for supressing all exceptions
+#     """
+#     def wrapper(*func_args, **func_kwargs):
+#         try:
+#             return function(*func_args, **func_kwargs)
+#         except:
+#             pass
+#     return wrapper
+
+@contextmanager
+def ignore_exceptions(*exceptions):
+    try:
+        yield
+    except exceptions:
+        pass
+
+
+if __name__ == "__main__":
+    with ignore_exceptions(ValueError):
+        int('string')
